51dff0ea6f1aeb2ac18de1aa2138aca628f775bf
Adam <adam@talsma.ca>
2014-02-01 21:40:27 -0800

    moved things around, added some comments, improved tests

--- src/ddate.py
+++ src/ddate.py
@@ -2,10 +2,19 @@
 
 Usage Examples:
 
-    from ddate import DDate
-
-    print(DDate())
-    print(DDate(datetime.date(year=2014, month=4, day=20)))
+    >>> from ddate import DDate
+    >>>
+    >>> DDate()
+    <src.ddate.DDate object at 0x7f3a6b88eb50>
+    <DDate date: 2014-02-01, day_of_season: 32, day_of_week: 1, holiday: None,
+    season: 0, year: 3180>
+    >>>
+    >>> print(DDate())
+    Today is Boomtime, the 32nd day of Chaos in the YOLD 3180
+    >>>
+    >>> import datetime
+    >>> print(DDate(datetime.date(year=2014, month=4, day=20)))
+    Setting Orange, the 37th day of Discord in the YOLD 3180
 """
 
 
@@ -15,98 +24,105 @@ import datetime
 class DDate(object):
     """Discordian Date Object.
 
-    Methods:
+    Methods::
+
+        __repr__: returns the typical repr plus the ddate attributes
         __str__: returns a formatted Discordian date string
 
     Attributes::
 
         date: the datetime.[date|datetime] object used to initialize with
-        day_of_week: the integer day of the week (0-5)
-        day_of_season: the integer day of the season (1-73)
+        day_of_week: the ordinal integer day of the week (0-5), or None
+        day_of_season: the cardinal integer day of the season (1-73), or None
         holiday: string holiday currently, or None
-        holidays: dict of Discordian holidays, {type: [holidays_per_season]}
-        season: integer season number
-        seasons: list of strings, Discordian seasons
-        weekdays: list of strings, Discordian days of the week
+        season: ordinal integer season number (0-5)
         year: integer Discordian YOLD
+
+    Statics::
+
+        HOLIDAYS: dict of Discordian holidays, {type: [holidays_per_season]}
+        SEASONS: list of strings, Discordian seasons
+        WEEKDAYS: list of strings, Discordian days of the week
     """
+
+    HOLIDAYS = {
+        "apostle": [
+            "Mungday",
+            "Mojoday",
+            "Syaday",
+            "Zaraday",
+            "Maladay",
+        ],
+        "seasonal": [
+            "Chaoflux",
+            "Discoflux",
+            "Confuflux",
+            "Bureflux",
+            "Afflux",
+        ],
+    }
+    SEASONS = [
+        "Chaos",
+        "Discord",
+        "Confusion",
+        "Bureaucracy",
+        "The Aftermath",
+    ]
+    WEEKDAYS = [
+        "Sweetmorn",
+        "Boomtime",
+        "Pungenday",
+        "Prickle-Prickle",
+        "Setting Orange",
+    ]
 
     def __init__(self, date=None, *args, **kwargs):
         """Discordian date setup and mangling.
 
         Args:
-            date: optional, date object with a timetuple method, or uses now
+            date: optional date object with a timetuple method, or uses today
         """
 
         if date is None or not hasattr(date, "timetuple"):
             date = datetime.date.today()
-
         self.date = date
 
         time_tuple = self.date.timetuple()
-        year = time_tuple.tm_year
-        day_of_year = time_tuple.tm_yday - 1
 
+        # calculate leap year using tradtional methods to align holidays
+        year = time_tuple.tm_year
         is_leap_year = (
             (year % 100 != 0 and year % 4 == 0) or
             (year % 100 == 0 and year % 400 == 0)
         )
+        self.year = year + 1166  # then adjust accordingly and assign
 
-        day_of_year_leap_corrected = day_of_year
+        day_of_year = time_tuple.tm_yday - 1  # ordinal
         if is_leap_year and day_of_year > 59:
-            day_of_year_leap_corrected -= 1
+            day_of_year -= 1  # St. Tib's doesn't count
 
-        self.day_of_season = (day_of_year_leap_corrected % 73) + 1
-        self.day_of_week = day_of_year_leap_corrected % 5
-        self.season = int(day_of_year_leap_corrected / 73)
-        self.year = year + 1166
+        self.day_of_week = day_of_year % 5
+        self.day_of_season = day_of_year % 73 + 1  # cardinal
+        self.season = int(day_of_year / 73)
 
-        self.seasons = [
-            "Chaos",
-            "Discord",
-            "Confusion",
-            "Bureaucracy",
-            "The Aftermath",
-        ]
-        self.weekdays = [
-            "Sweetmorn",
-            "Boomtime",
-            "Pungenday",
-            "Prickle-Prickle",
-            "Setting Orange",
-        ]
-        self.holidays = {
-            "apostle": [
-                "Mungday",
-                "Mojoday",
-                "Syaday",
-                "Zaraday",
-                "Maladay",
-            ],
-            "seasonal": [
-                "Chaoflux",
-                "Discoflux",
-                "Confuflux",
-                "Bureflux",
-                "Afflux",
-            ],
-        }
-
-        if is_leap_year and day_of_year == 59:
+        if is_leap_year and time_tuple.tm_yday == 60:
             self.holiday = "St. Tib's Day"
+            self.day_of_week = None
+            self.day_of_season = None
         elif self.day_of_season == 5:
-            self.holiday = self.holidays["apostle"][self.season]
+            self.holiday = self.HOLIDAYS["apostle"][self.season]
         elif self.day_of_season == 50:
-            self.holiday = self.holidays["seasonal"][self.season]
+            self.holiday = self.HOLIDAYS["seasonal"][self.season]
         else:
             self.holiday = None
 
         super(DDate, self).__init__(*args, **kwargs)
 
     def __str__(self):
-        """String formatting for the current date."""
+        """Return a formatted string for the current date."""
 
-        today = 0 <= (self.date - datetime.date.today()).days < 1
+        today = self.date.timetuple().tm_yday == datetime.date.today(
+            ).timetuple().tm_yday
 
         if self.holiday == "St. Tib's Day":
             return "{today}{self.holiday}, {self.year} YOLD".format(
@@ -116,34 +132,45 @@ class DDate(object):
         else:
             return (
                 "{today}{day}, the {self.day_of_season}{pfix}"
-                " day of {season} in the YOLD {self.year}"
-                "{celebrate}{holiday}{exclamation}"
+                " day of {season} in the YOLD {self.year}{holiday}"
             ).format(
                 today="Today is " if today else "",
-                day=self.weekdays[self.day_of_week],
+                day=self.WEEKDAYS[self.day_of_week],
                 self=self,
                 pfix=_day_postfix(self.day_of_season),
-                season=self.seasons[self.season],
-                celebrate=". Celebrate " if self.holiday else "",
-                holiday=self.holiday or "",
-                exclamation="!" if self.holiday else "",
+                season=self.SEASONS[self.season],
+                holiday=". Celebrate {}!".format(
+                    self.holiday) if self.holiday else "",
             )
+
+    def __repr__(self):
+        """Return our id and attributes."""
+
+        attributes = []
+        for attr in dir(self):
+            if not attr.startswith("_") and attr.lower() == attr:
+                attributes.append(
+                    "{attr}: {{self.{attr}}}".format(attr=attr)
+                )
+
+        return "<{name}.{cls} object at {id}>\n<{cls} {attributes}>".format(
+            name=__name__,
+            cls=self.__class__.__name__,
+            id=hex(id(self)),
+            attributes=", ".join(attributes).format(self=self),
+        )
 
 
 def _day_postfix(day):
     """Returns day's correct postfix (2nd, 3rd, 61st, etc)."""
 
     if day != 11 and day % 10 == 1:
-        pfix = "st"
+        postfix = "st"
     elif day != 12 and day % 10 == 2:
-        pfix = "nd"
+        postfix = "nd"
     elif day != 13 and day % 10 == 3:
-        pfix = "rd"
+        postfix = "rd"
     else:
-        pfix = "th"
+        postfix = "th"
 
-    return pfix
-
-
-if __name__ == "__main__":
-    print(DDate())
+    return postfix
--- test/ddate_tests.py
+++ test/ddate_tests.py
@@ -4,60 +4,75 @@
 import datetime
 import unittest
 
-import ddate
+try:
+    import ddate
+except ImportError:
+    import sys
+    sys.path.insert(1, "..")
+    import src as ddate
 
 
 class DDateTests(unittest.TestCase):
-    def test_specifc_leap_day(self):
-        ddate_2012420 = ddate.DDate(datetime.date(year=2012, month=4, day=20))
-        self.assertEqual(
-            ddate_2012420.weekdays[ddate_2012420.day_of_week],
-            "Setting Orange",
-        )
-        self.assertEqual(ddate_2012420.day_of_season, 37)
-        self.assertEqual(
-            ddate_2012420.seasons[ddate_2012420.season],
-            "Discord",
-        )
+    def test_specifc_leap_year_day(self):
+        leap = ddate.DDate(datetime.date(year=2012, month=4, day=20))
+        self.assertEqual(leap.WEEKDAYS[leap.day_of_week], "Setting Orange")
+        self.assertEqual(leap.SEASONS[leap.season], "Discord")
+        self.assertEqual(leap.day_of_season, 37)
 
-    def test_specific_non_leap_day(self):
-        ddate_now = ddate.DDate(datetime.date(year=2014, month=2, day=1))
-        self.assertEqual(
-            ddate_now.weekdays[ddate_now.day_of_week],
-            "Boomtime",
-        )
-        self.assertEqual(ddate_now.day_of_season, 32)
-        self.assertEqual(
-            ddate_now.seasons[ddate_now.season],
-            "Chaos",
-        )
+    def test_specific_non_leap_year_day(self):
+        non_leap = ddate.DDate(datetime.date(year=2014, month=2, day=1))
+        self.assertEqual(non_leap.WEEKDAYS[non_leap.day_of_week], "Boomtime")
+        self.assertEqual(non_leap.SEASONS[non_leap.season], "Chaos")
+        self.assertEqual(non_leap.day_of_season, 32)
 
-    def test_string_formatting(self):
-        ddate_now = ddate.DDate(datetime.date(year=2014, month=2, day=1))
-        self.assertTrue(str(ddate_now).endswith(
-            "Boomtime, the 32nd day of Chaos in the YOLD 3180",
-        ))
-
-    def test_holiday_string_formatting(self):
-        ddate_now = ddate.DDate(datetime.date(year=2014, month=2, day=19))
-        self.assertTrue(str(ddate_now).endswith(
+    def test_discordian_holiday(self):
+        holiday = ddate.DDate(datetime.date(year=2014, month=2, day=19))
+        self.assertEqual(holiday.holiday, "Chaoflux")
+        self.assertTrue(str(holiday).endswith(
             "Setting Orange, the 50th day of Chaos in the YOLD 3180. "
             "Celebrate Chaoflux!",
         ))
 
-    def test_st_tibs_day(self):
-        ddate_tibs = ddate.DDate(datetime.date(year=2012, month=2, day=29))
+    def test_st_tibs_day_eve(self):
+        st_tibs_eve = ddate.DDate(datetime.date(year=2012, month=2, day=28))
         self.assertEqual(
-            str(ddate_tibs),
-            "St. Tib's Day, 3178 YOLD",
+            str(st_tibs_eve),
+            "Prickle-Prickle, the 59th day of Chaos in the YOLD 3178"
         )
 
+    def test_st_tibs_day(self):
+        st_tibs_day = ddate.DDate(datetime.date(year=2012, month=2, day=29))
+        self.assertEqual(str(st_tibs_day), "St. Tib's Day, 3178 YOLD")
+        self.assertEqual(st_tibs_day.day_of_season, None)
+        self.assertEqual(st_tibs_day.day_of_week, None)
+
     def test_day_after_st_tibs_day(self):
-        ddate_tibs = ddate.DDate(datetime.date(year=2012, month=3, day=1))
+        post_st_tibs = ddate.DDate(datetime.date(year=2012, month=3, day=1))
         self.assertEqual(
-            str(ddate_tibs),
+            str(post_st_tibs),
             "Setting Orange, the 60th day of Chaos in the YOLD 3178"
         )
+
+    def test_new_years_eve(self):
+        nye = ddate.DDate(datetime.datetime(year=2014, month=12, day=31))
+        self.assertEqual(nye.WEEKDAYS[nye.day_of_week], "Setting Orange")
+        self.assertEqual(nye.SEASONS[nye.season], "The Aftermath")
+        self.assertEqual(nye.day_of_season, 73)
+        self.assertEqual(nye.year, 3180)
+
+    def test_new_years_day(self):
+        nyd = ddate.DDate(datetime.datetime(year=2015, month=1, day=1))
+        self.assertEqual(nyd.WEEKDAYS[nyd.day_of_week], "Sweetmorn")
+        self.assertEqual(nyd.SEASONS[nyd.season], "Chaos")
+        self.assertEqual(nyd.day_of_season, 1)
+        self.assertEqual(nyd.year, 3181)
+
+    def test_data_in_repr(self):
+        ddate_obj = ddate.DDate(datetime.date(year=2012, month=2, day=29))
+        self.assertIn(str(hex(id(ddate_obj))), repr(ddate_obj))
+        for attr in dir(ddate_obj):
+            if not attr.startswith("_") and attr.lower() == attr:
+                self.assertIn(str(getattr(ddate_obj, attr)), repr(ddate_obj))
 
 
 if __name__ == "__main__":
