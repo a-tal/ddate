da8a4324ace19a04f49ac9213fe19246b4b95eb8
Adam <adam@talsma.ca>
2014-02-01 22:49:57 -0800

    improved the cmd line entry

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="ddate",
-    version="0.0.1",
+    version="0.0.2",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     package_dir={"ddate": "src"},
