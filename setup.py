19797d5bd7bddd7ef94fe3292aa7f2225281a726
a-tal <github@talsma.ca>
2014-03-09 16:26:57 -0700

    version 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ddate",
-    version="0.0.3",
+    version="0.0.4",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     packages=["ddate"],
