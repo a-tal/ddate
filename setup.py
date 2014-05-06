0ecf7cacb6daae099d1f4f4c0fa77aa215aba2db
a-tal <github@talsma.ca>
2014-05-06 09:25:46 -0700

    moved to pytest, added pypy to travis build

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """Shim in pytest to be able to use it with setup.py test."""
+
+    def finalize_options(self):
+        """Stolen from http://pytest.org/latest/goodpractises.html."""
+
+        TestCommand.finalize_options(self)
+        self.test_args = ["-v", "-rf", "--cov", "ddate"]
+        self.test_suite = True
+
+    def run_tests(self):
+        """Also shamelessly stolen."""
+
+        # have to import here, outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
 
 
 setup(
     name="ddate",
-    version="0.0.4",
+    version="0.0.5",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     packages=["ddate"],
@@ -14,8 +34,8 @@ setup(
         "Converts and prints time using the Discordian calendar"
     ),
     download_url="https://github.com/a-tal/ddate",
-    tests_require=['nose'],
-    test_suite='nose.collector',
+    cmdclass={"test": PyTest},
+    tests_require=["pytest", "pytest-cov"],
     license="BSD",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
