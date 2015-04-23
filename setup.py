cd8d0167a423e9a324dbd42ae0c46d6c09b2193e
a-tal <github@talsma.ca>
2015-04-23 16:13:40 -0700

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
 
 setup(
     name="ddate",
-    version="0.0.5",
+    version="0.0.6",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     packages=["ddate"],
