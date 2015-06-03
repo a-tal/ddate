4a19f34c0184ee829fdcb18f6092f1f9dbb59fa8
a-tal <github@talsma.ca>
2015-06-02 21:18:28 -0700

    0.0.8 - automatically deployed from travis

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
 
 setup(
     name="ddate",
-    version="0.0.7",
+    version="0.0.8",
     author="Adam Talsma",
     author_email="adam@talsma.ca",
     packages=["ddate"],
